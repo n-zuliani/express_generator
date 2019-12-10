a1b87acba5ae5ab8de188f741902faf0b5039955
n-zuliani <zuliani.nicolo@iisgalvanimi.edu.it>
2019-12-10 12:47:44 +0000

    3.1

---
+++ EXPRESS_GENERATOR/routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/ciaone', function(req, res, next) {
+  res.send('PRONTONE!!!!');
+});
+
+
+module.exports = router;
